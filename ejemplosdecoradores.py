a25041ed77b14c1a0eafbbdfdf3b3927bce73665
violetamdm <122279486+violetamdm@users.noreply.github.com>
2023-03-10 13:24:28 +0100

    Update ejemplosdecoradores.py

--- ejemplosdecoradores.py
+++ ejemplosdecoradores.py
@@ -1,4 +1,18 @@
 # DECORADORES:
+"""
+Los decoradores son funciones que "gestionan" a otras 
+funciones antes de llamarlas.
+"""
+
+## SINTAXIS:
+def decorador(funcion):
+    pass
+
+@decorador
+def funcion_a_decorar():
+    pass
+
+## EJEMPLO CON PRINT:
 def my_decorator(func):
     def wrapper():
         print("Something is happening before the function is called.")
@@ -11,3 +25,21 @@ def say_whee():
     print("Whee!")
 
 my_decorator("hola")
+
+## EJEMPLO CON TRY:
+def decorador(funcion):
+    def funcion_aux_del_decorador():
+        x=""
+        try:
+            print('esto petará')
+        except NameError:
+            x = "Variable x is not defined"
+        except:
+            x = "Something else went wrong"
+        return x
+    return funcion_aux_del_decorador()
+    
+@decorador
+def funcion_a_decorar():
+    pass
+
